68ca3290fa7a7712a0d0f1d0e71845e03b19c0e9
yxf <yxf4559@163.com>
2020-07-24 14:24:16 +0800

    Update kusama genesis hash

--- src/networks.rs
+++ src/networks.rs
@@ -1,6 +1,6 @@
 
 pub const POLKADOT_GENESIS_HASH: &'static str = "0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
-pub const KUSAMA_GENESIS_HASH: &'static str = "0x92b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
+pub const KUSAMA_GENESIS_HASH: &'static str = "0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe";
 
 #[derive(Clone, Eq, PartialEq, Debug, Copy)]
 pub enum Network {
